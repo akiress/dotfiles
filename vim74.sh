804d3c8cec78dfd6eb302ce34e8304d721c2a456
akiress <akiress1976@gmail.com>
2014-03-04 11:22:32 -0600

    Update

--- vim74.sh
+++ vim74.sh
@@ -5,4 +5,4 @@ wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar -xjf vim-7.4.tar.bz2
 cd vim74
 
-./configure --prefix=/usr --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-python3interp --enable-luainterp --enable-perlinterp
+./configure --prefix=/usr/local --with-features=huge --enable-rubyinterp --enable-pythoninterp --enable-python3interp --enable-luainterp --enable-perlinterp --enable-cscope --enable-largefiles
